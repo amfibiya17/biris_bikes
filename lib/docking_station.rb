e908c3df678a5e282774325220f2925f0359c4d7
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-04-22 10:32:10 +0100

    step 13

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,15 @@
-require_relative 'bike'
+require_relative "bike"
 
 class DockingStation
   attr_reader :bike
   
   def dock(bike)
+    fail "Docking station full" if @bike
     @bike = bike
   end
 
   def release_bike
-    fail 'No bikes available' unless @bike
+    fail "No bikes available" unless @bike
     @bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,45 +1,49 @@
-require 'docking_station'
+require "docking_station"
 
 describe DockingStation do
 
-  # describe '#release_bike' do
-  # it 
-
-
   it { is_expected.to respond_to :release_bike }
 
-  it 'get bike and check it works' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  it "get bike and check it works" do
+    station = DockingStation.new
+    bike = Bike.new
+    station.dock(bike)
+    bike = station.release_bike
+    expect(bike.working?).to eq(true)
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it { is_expected.to respond_to(:bike) }
 
-  it 'docks something' do
+  it "docks something" do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
 
-  it 'returns docked bikes' do
+  it "returns docked bikes" do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
 
-  describe '#release_bike' do
-    it "raises error if no bikes available" do
-      expect { subject.release_bike }.to raise_error 'No bikes available'
-    end
-    
-    it 'releases a bike' do
+  describe "#release_bike" do
+    it "releases a bike" do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
+    it "raises error if no bikes available" do
+      expect { subject.release_bike }.to raise_error "No bikes available"
+    end
   end
 
+  describe "#dock" do
+    it "raises an error when full" do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error "Docking station full"
+    end
+  end
 end
 
   
