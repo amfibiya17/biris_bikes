344214fa602b3b87d6c7fa61a8cfd4698fcd6bf0
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-04-22 11:19:21 +0100

    test errors step 14

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
-  it "get bike and check it works" do
+  it "Get bike and check it works" do
     station = DockingStation.new
     bike = Bike.new
     station.dock(bike)
@@ -16,32 +16,33 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:bike) }
 
-  it "docks something" do
+  it "Docks something" do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
 
-  it "returns docked bikes" do
+  it "Returns docked bikes" do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
 
   describe "#release_bike" do
-    it "releases a bike" do
+    it "Releases a bike" do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
-    it "raises error if no bikes available" do
+    it "Raises error if no bikes available" do
       expect { subject.release_bike }.to raise_error "No bikes available"
     end
   end
 
   describe "#dock" do
-    it "raises an error when full" do
-      subject.dock(Bike.new)
-      expect { subject.dock Bike.new }.to raise_error "Docking station full"
+    it "Raises an error when full" do
+      station = DockingStation.new
+      20.times { station.dock(Bike.new) }
+      expect { station.dock Bike.new }.to raise_error "Docking station full"
     end
   end
 end
